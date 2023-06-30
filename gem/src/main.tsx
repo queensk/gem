0379d0eb1d5bfb55f5ddf0bc83b514eec80adba0
queens kisivuli <queenskisivuli@gmail.com>
2023-06-30 13:37:52 +0300

    base main

--- gem/src/main.tsx
+++ gem/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { AuthProvider } from "./UseContext/UseAuth/UseAuth";
+import SearchMessageUser from "./UseContext/UseMessage/SearchMessageUser.tsx";
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  // <React.StrictMode>
+  <AuthProvider>
+    <SearchMessageUser>
+      <App />
+    </SearchMessageUser>
+  </AuthProvider>
+  // </React.StrictMode>
+);
